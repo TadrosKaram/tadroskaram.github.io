777b18ee066ced473f8c54dbd9ec0959de0940a6
TadrosKaram <2100425@eng.asu.edu.eg>
2025-07-07 08:13:17 +0300

    major

---
+++ script.js
@@ -0,0 +1,8 @@
+//Pointer effect
+
+const circle = document.querySelector(".circle");
+
+document.addEventListener("mousemove", (e) => {
+  circle.style.left = `${e.clientX}px`;
+  circle.style.top = `${e.clientY}px`;
+});
