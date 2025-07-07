e1a91de49e74917f743d11d516af7434c1d3688c
TadrosKaram <2100425@eng.asu.edu.eg>
2025-07-07 10:13:39 +0300

    atr

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ document.addEventListener("mousemove", (e) => {
   circle.style.left = `${e.clientX}px`;
   circle.style.top = `${e.clientY}px`;
 });
+
+///////////////////////////////////////
+//nav bar hamburger btn
+
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+//open
+hamburgerBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+//////////////////////////////////////
+//scroll up button
+
+const scrollUp = document.querySelector("#thescroll");
+window.addEventListener("scroll", function () {
+  let isClicked = false;
+  if (window.scrollY > 500) {
+    scrollUp.classList.remove("hide-scroll");
+    isClicked = true;
+  } else if (window.scrollY == 0) {
+    scrollUp.classList.add("hide-scroll");
+  }
+  console.log(window.scrollY);
+});
