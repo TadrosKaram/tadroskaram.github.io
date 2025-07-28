5f6616b760f0c33998171a6810c187575defb121
TadrosKaram <2100425@eng.asu.edu.eg>
2025-07-28 06:25:26 +0300

    dark-light

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//Pointer effect
-
-const circle = document.querySelector(".circle");
-
-document.addEventListener("mousemove", (e) => {
-  circle.style.left = `${e.clientX}px`;
-  circle.style.top = `${e.clientY}px`;
-});
-
-///////////////////////////////////////
 //nav bar hamburger btn
 
 const hamburgerBtn = document.getElementById("hamburger-btn");
